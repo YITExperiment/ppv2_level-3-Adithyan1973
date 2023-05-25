c91d3601c3d6e9feb800b35bc99d8fd44bf49747
Anchalan-Adithyan <134507262+Adithyan1973@users.noreply.github.com>
2023-05-25 17:52:42 +0530

    Add files via upload

---
+++ kaleido_spiral.py.py
@@ -0,0 +1,14 @@
+import turtle
+
+
+def draw_circle(size):
+    turtle.pencolor('red')
+    turtle.circle(size)
+    draw_circle(size)
+
+
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(4)
+draw_circle(30)
+
